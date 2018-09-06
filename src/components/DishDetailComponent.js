9afc679c6ae16c6b5ad2371d59563726f9b26127
Medi Risasi <morisasy@gmail.com>
2018-09-06 15:16:22 +0300

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,12 +7,12 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 class  DishDetail extends Component{
         
     renderComments(comments){
-             if (this.props.dish) {
-            const dishComments =this.props.dish.comments.map((comment) => {
+             if (comments) {
+            const dishComments = comments.map((comment) => {
                 return (
                     <li key={comment.id}>        
                               <p>{comment.comment}</p>        
-                              <p>-- {comment.author},{(new Date(comment.date)).toDateString} </p>        
+                              <p>-- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>        
                      </li>     
 
                 );
@@ -25,29 +25,32 @@ class  DishDetail extends Component{
         renderDish(dish){
             
             if (this.props.dish) {
+                console.log("comments: ", this.props.dish.comments);
                
-                const selectedComments = this.renderComments(this.props.dish);
+                const selectedComments = this.renderComments(this.props.dish.comments);
                 return(
-                    
-                    <div className = "row">
-                    
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>     
-                    </div>
-                
-                    <div  className="col-12 col-md-5 m-1">
-                        <ul className="list-unstyled">
-                            <h4>Comments</h4>
-                            {selectedComments}
-                        </ul>                                     
-                    </div>
-                </div>
+                   <div className="container">
+                        
+                        <div className = "row">
+                            
+                            <div  className="col-12 col-md-5 m-1">
+                                <Card>
+                                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                                    <CardBody>
+                                    <CardTitle>{this.props.dish.name}</CardTitle>
+                                    <CardText>{this.props.dish.description}</CardText>
+                                    </CardBody>
+                                </Card>     
+                            </div>
+                        
+                            <div  className="col-12 col-md-5 m-1">
+                                <ul className="list-unstyled">
+                                    <h4>Comments</h4>
+                                    {selectedComments}
+                                </ul>                                     
+                            </div>
+                        </div>
+                   </div>
                 );
 
             }
